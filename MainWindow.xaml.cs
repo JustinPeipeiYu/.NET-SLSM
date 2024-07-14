4adf56fa0dd9505a87298a185bfc32782cb1b739
Justin Yu <justinyu6866@gmail.com>
2024-07-14 16:23:02 -0400

    Day 10 - Reduce number of text files from 5 to 2, write data to text files

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel;
 using System.Diagnostics.Eventing.Reader;
 using System.Diagnostics.Metrics;
+using System.DirectoryServices;
 using System.Drawing;
 using System.Globalization;
 using System.IO;
@@ -22,6 +23,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Xml.Linq;
 using static System.Formats.Asn1.AsnWriter;
+using static System.Net.Mime.MediaTypeNames;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace SLSM
@@ -31,175 +33,82 @@ namespace SLSM
     /// </summary>
     public partial class MainWindow : Window
     {
-        //protected int index, hoursToBeat, sizeOfList, index2;
+        //path to text files
         string path = System.IO.Path.GetDirectoryName(
                 System.Reflection.Assembly.GetEntryAssembly().Location);
-        /// <summary>
-        /*Lists of data*/
-        /// </summary>
+        
+        /*LISTS TO STORE DATASETS AND USER DATA*/
         List<DateTime> dates = new List<DateTime>();
         List<string> brands = new List<string>();
         List<float> standardPrices = new List<float>();
         List<float> largePrices = new List<float>();
         List<float> spendingAmount = new List<float>();
         Dictionary<string, Tuple<float, float>> inventory = new Dictionary<string, Tuple<float, float>>();
-        //index to get price from inventory (index = 0 is small, index = 1 is large)
-        int index = -1;
-        //protected bool after;
-        //Tuple<string, string> output;
-        //Tuple<int, int> seeds;
-        //CultureInfo provider;
-        //int numOfPacks = 0;
 
+        //sizeIndex stores price from inventory (sizeIndex = 0 is small, sizeIndex = 1 is large)
+        int sizeIndex = -1;
+        string brandSelected = "";
 
-        //public MainWindow() => InitializeComponent();
-
-        /*
-            path = System.IO.Path.GetDirectoryName(
-                System.Reflection.Assembly.GetEntryAssembly().Location); ;//relative directory path
-            */
-        /*
-         * !int.TryParse(Textbox1.Text, out parsedValue) //try to parse a textbox input
-         */
-        /*
-         * Convert.ToInt32((date2 - date1).TotalHours) //convert date to int
-         * */
-        /*
-         * using (StreamWriter sw = File.AppendText(path)) //write to text file
-            {
-                sw.WriteLine(entry1);
-                sw.Close();
-            }
-         */
-        /*
-        protected List<string> separateEntry(string entries) //separating entries in string 
-        {
-            string sEntry="";
-            provider = new CultureInfo("en-US");
-            List<string> entryList = new List<string>();
-            foreach (char c in entries)
-            {
-                if (c != '\n' && c!='\r')
-                {
-                    sEntry += c;
-                } else if (c == '\n' && !sEntry.Equals(""))
-                {
-                    
-                        entryList.Add(sEntry);
-                    
-                    sEntry = "";
-                }
-            }
-            if (!sEntry.Equals(""))
-            {
-                entryList.Add(sEntry);
-            }
-                
-            return entryList;
-        }
-        */
-        /*                  
-            string newWord="";                          //reverse a word
-            for (int i=word.Length-1; i>=0; i--)
-            {
-                newWord += word.Substring(i, 1);
-            }
-            return newWord;
-            */
-        /*
-         * using (StreamReader sr = new StreamReader(System.IO.Path.Combine(path, "seed.txt"))) //read from file, capture last entry
-                {
-                    first = sr.ReadLine();
-                    whole=sr.ReadToEnd();
-                    index = whole.LastIndexOf("\n");
-                    last = whole.Substring(index+1);
-                    sr.Close();
-                }
-         */
-
-        /*
-         * if (File.Exists(System.IO.Path.Combine(path, "dates.txt"))) //check if file exists, if not create
-            {
-                provider = new CultureInfo("en-US");
-                using (StreamReader sr = new StreamReader(System.IO.Path.Combine(path, "dates.txt")))
-            } else {
-                write("", System.IO.Path.Combine(path, "dates.txt"));//update date
-            }
-         */
-        /*
-         * 
-            days = Convert.ToInt32(hours / 24); //convert from one unit to another
-         */
+        //datetime culture
+        CultureInfo culture = new CultureInfo("en-US");
+        
+        /*MAIN PROGRAM*/
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            //today's date
-
-            //seeds = readSeed();
-            //readDate(false);
-
-            // Handle opening logic, NOTE: reading from text file requires a literal path instead of a variable path
-            using (StreamReader sr = new StreamReader(System.IO.Path.Combine(path, "brandNames.txt"))) //read from file, capture last entry
-            {
-                string line;
-                while ((line = sr.ReadLine()) != "" && line != null)
-                {
-                    brands.Add(line);
-                }
-                sr.Close();
-            }
-            using (StreamReader sr = new StreamReader(System.IO.Path.Combine(path, "spendingAmounts.txt")))
-            {
-                string line;
-                while ((line = sr.ReadLine()) != "" && line != null)
-                {
-                    var price = float.Parse(line);
-                    spendingAmount.Add(price);
-                }
-                sr.Close();
-            }
-            using (StreamReader sr = new StreamReader(System.IO.Path.Combine(path, "standardPrices.txt"))) 
-            {
-                string line;
-                while ((line = sr.ReadLine()) != "" && line != null)
-                {
-                    var price = float.Parse(line);
-                    standardPrices.Add(price);
-                }
-                sr.Close();
-            }
-            using (StreamReader sr = new StreamReader(System.IO.Path.Combine(path, "largePrices.txt"))) 
-            {
-                string line;
-                while ((line = sr.ReadLine()) != "" && line != null)
-                {
-                    var price = float.Parse(line);
-                    largePrices.Add(price);
-                }
-                sr.Close();
-            }
-            using (StreamReader sr = new StreamReader(System.IO.Path.Combine(path, "firstDate.txt"))) 
-            {
-                string line;
-                while ((line = sr.ReadLine()) != "" && line != null)
-                {
-                    dates.Add(DateTime.Parse(line));
-                }
-                sr.Close();
-            }
-            populateInventory(inventory,brands,standardPrices,largePrices);
+            // Handle opening logic
+            readDates();
+            readPriceByBrand();
         }
         private void OnWindowClosing(object sender, CancelEventArgs e)
         {
             // Handle closing logic, set e.Cancel as needed
         }
 
-        private Dictionary<string, Tuple<float, float>> populateInventory(Dictionary<string, Tuple<float, float>> inventory, List<string> brands,List<float> standardPrices,List<float> largePrices)
+        /*CUSTOM FUNCTIONS*/
+        private void readPriceByBrand()
+        {
+            //NOTE: reading from text files requires a literal path instead of a variable path
+            using (StreamReader sr = new StreamReader(System.IO.Path.Combine(path, "priceByBrand.txt"))) //read from file, capture last entry
+            {
+                string line;
+                while ((line = sr.ReadLine()) != "" && line != null)
+                {
+                    string[] entries = line.Split(",");
+                    brands.Add(entries[0].Trim());
+                    standardPrices.Add(float.Parse(entries[1].Trim()));
+                    largePrices.Add(float.Parse(entries[2].Trim()));
+                }
+                sr.Close();
+            }
+            populateInventory();
+            return;
+        }
+        private void readDates()
+        {
+            //NOTE: reading from text files requires a literal path instead of a variable path
+            using (StreamReader sr = new StreamReader(System.IO.Path.Combine(path, "dates.txt")))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != "" && line != null)
+                {
+                    string[] entries = line.Split(",");
+                    dates.Add(DateTime.ParseExact(entries[0].Trim(),"d",culture));
+                    entries = entries.Skip(1).ToArray();
+                    foreach (string entry in entries)
+                    {
+                        spendingAmount.Add(float.Parse(entry.Trim()));
+                    }
+                }
+                sr.Close();
+            }
+            return;
+        }
+        private void populateInventory()
         {
             foreach (var brand in brands){
                 int i = brands.IndexOf(brand);
                 inventory[brand] = new Tuple<float, float>(standardPrices[i], largePrices[i]);
             }
-            return inventory;
         }
 
         private MessageBoxResult confirmSubmission()
@@ -213,16 +122,58 @@ namespace SLSM
             return result;
         }
 
+        private void writeDate()
+        {
+            /*NOTE: writing to text file requires literal path instead of variable path*/
+            using (StreamWriter sw = new StreamWriter(System.IO.Path.Combine(path, "dates.txt"), true))
+            {
+                //if the file is not blank, write date on new line
+                if (dates.Count > 0)
+                {
+                    //if the last date entry was today, do not write new date
+                    if (dates.Last().Equals(DateTime.Today.Date))
+                    {
+                        sw.Close();
+                        return;
+                    }
+                    sw.WriteLine("");
+                    sw.Write(DateTime.Now.ToString("d", culture));
 
+                } else //otherwise write date on first line
+                {
+                    sw.Write(DateTime.Now.ToString("d", culture));
+                }
+                sw.Close();
+            }
+            return;
+        }
+
+        private void writePrice(string brandName, int sizeIndex)
+        {
+            if (sizeIndex==0)
+            {
+                /*NOTE: writing to text file requires literal path instead of variable path*/
+                using (StreamWriter sw = new StreamWriter(System.IO.Path.Combine(path, "dates.txt"),true))
+                {
+                    sw.Write(","+inventory[brandName].Item1);
+                    sw.Close();
+                }
+            } else
+            {
+                using (StreamWriter sw = new StreamWriter(System.IO.Path.Combine(path, "dates.txt"), true))
+                {
+                    sw.Write(","+inventory[brandName].Item2);
+                    sw.Close();
+                }
+            }
+            return;
+        }
+
+        /*DYNAMIC METHODS*/
         private void btnSubmit_Click(object sender, RoutedEventArgs e)
         {
-            /*If there entry, do not write to file*/
-            if (dates.Count > 0)
-            {
-                return;
-            }
             /*If no radio button selected, do not write to file*/
-            if (index == -1)
+            if (sizeIndex == -1)
             {
                 return;
             }
@@ -231,16 +182,16 @@ namespace SLSM
             {
                 return;
             }
+            //submission procedure when submit button is clicked
             switch (confirmSubmission())
             {
                 case MessageBoxResult.Yes:
-                    /*NOTE: writing to text file requires literal path instead of variable path*/
-                    using (StreamWriter sw = new StreamWriter(System.IO.Path.Combine(path, "firstDate.txt")))
-                    {
-                        sw.WriteLine(DateTime.Now.ToString("d/M/yyyy"));
-                        sw.Close();
-                    }
+                    //save changes
+                    writeDate();
+                    writePrice(brandSelected, sizeIndex);
                     MessageBox.Show("Changes were saved.");
+                    //update program variables
+                    readDates();
                     break;
                 case MessageBoxResult.No:
                     break;
@@ -249,18 +200,28 @@ namespace SLSM
 
         private void btnSubmit_Selected(object sender, RoutedEventArgs e, KeyEventArgs s)
         {
+            //submission procedure when spacebar is pressed
             if (s.Key == Key.Space)
             {
+                /*If no radio button selected, do not write to file*/
+                if (sizeIndex == -1)
+                {
+                    return;
+                }
+                /*If no brand selected, do not write to file*/
+                if (lbBrand.SelectedItem == null)
+                {
+                    return;
+                }
                 switch (confirmSubmission())
                 {
                     case MessageBoxResult.Yes:
-                        /*NOTE: writing to text file requires literal path instead of variable path*/
-                        using (StreamWriter sw = new StreamWriter(System.IO.Path.Combine(path, "firstDate.txt")))
-                        {
-                            sw.WriteLine(DateTime.Now.ToString("d/M/yyyy"));
-                            sw.Close();
-                        }
+                        //save changes
+                        writeDate();
+                        writePrice(brandSelected, sizeIndex);
                         MessageBox.Show("Changes were saved.");
+                        //update program variables
+                        readDates();
                         break;
                     case MessageBoxResult.No:
                         break;
@@ -270,12 +231,72 @@ namespace SLSM
 
         private void rbRegular_Checked(object sender, RoutedEventArgs e)
         {
-            index = 0;
+            sizeIndex = 0;
         }
 
         private void rbKing_Checked(object sender, RoutedEventArgs e)
         {
-            index = 1;
+            sizeIndex = 1;
+        }
+
+        private void BensonAndHedges_Selected(object sender, RoutedEventArgs e)
+        {
+            brandSelected = "Benson and Hedges";
+        }
+
+        private void CanadianClassics_Selected(object sender, RoutedEventArgs e)
+        {
+            brandSelected = "Canadian Classics";
+        }
+
+        private void DuMaurier_Selected(object sender, RoutedEventArgs e)
+        {
+            brandSelected = "Du Maurier";
+        }
+
+        private void ExportA_Selected(object sender, RoutedEventArgs e)
+        {
+            brandSelected = "Export A";
+        }
+
+        private void JohnPlayerSpecial_Selected(object sender, RoutedEventArgs e)
+        {
+            brandSelected = "John Player Special";
+        }
+
+        private void LD_Selected(object sender, RoutedEventArgs e)
+        {
+            brandSelected = "LD";
+        }
+
+        private void MacdonaldSelect_Selected(object sender, RoutedEventArgs e)
+        {
+            brandSelected = "Macdonald Select";
+        }
+
+        private void Matinee_Selected(object sender, RoutedEventArgs e)
+        {
+            brandSelected = "Matinee";
+        }
+
+        private void NEXT_Selected(object sender, RoutedEventArgs e)
+        {
+            brandSelected = "NEXT";
+        }
+
+        private void Number7_Selected(object sender, RoutedEventArgs e)
+        {
+            brandSelected = "Number 7";
+        }
+
+        private void PallMall_Selected(object sender, RoutedEventArgs e)
+        {
+            brandSelected = "Pall Mall";
+        }
+
+        private void PhilipMorris_Selected(object sender, RoutedEventArgs e)
+        {
+            brandSelected = "Philip Morris";
         }
     }//end class
 
